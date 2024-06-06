ee3207577c7615086f23e7f4811eb9161869e64e
kush <119112478+ankush-singh2005@users.noreply.github.com>
2024-06-06 23:32:01 +0530

    Create selection sort.cpp

---
+++ selection sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={64,25,12,22,11};
+    for(int i=0;i<5;i++){
+        int min=i;
+        for(int j=i+1;j<5;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+        
+    }
+    cout<<"The sorted array is: "<<endl;
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+}
